4c9e6f7e6a4e1033d65a01a67a026e63b94fa109
alekgomes <lucas.alek93@gmail.com>
2018-10-05 10:01:25 -0300

    first commit

---
+++ aula-01/models.py
@@ -0,0 +1,49 @@
+class Filme:
+	def __init__(self, nome, ano, duracao):
+		self._nome = nome
+		self.ano  = ano
+		self.duracao = duracao
+		self._likes = 0
+
+	@property 	#getter
+	def likes(self):
+		return self._likes
+
+	def dar_like(self):
+		self._likes += 1
+
+	@property 	#getter
+	def nome(self):
+		return self._nome
+
+	@nome.setter #setter
+	def nome(self,nome):
+		self._nome = nome
+
+class Serie:
+	def __init__(self, nome, ano, temporadas):
+		self.nome = nome
+		self.ano  = ano
+		self.temporadas = temporadas
+
+	@property	#getter
+	def likes(self):
+		return self._likes
+
+	def dar_like(self):
+		self._likes += 1
+
+	@property 	#getter
+	def nome(self):
+		return self._nome
+
+	@nome.setter #setter
+	def nome(self,nome):
+		self._nome = nome
+
+		
+vingadores = Filme('vingadores - guerra infinita', '2018', 160)
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}')
+
+atlanta = Serie('atlanta', 2018, 2)
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Avançando na Orientação a Objetos em Python - Alura 
+
+Essa é a sequencia do curso de Introdução a Orientação a Objetos.
+É um curso com carga horária estimada em 10h, dividias em 6 aulas abrangendo os seguintes temas:
+* Herança
+* Polimorfismo 
+* Duck typing e modelo de dados
+* Herança Múltipla
+
+Ao contrário do curso introdutório, não criaremos uma aplição mas trabalharemos com trechos de código para melhor entender os conceitos. 
